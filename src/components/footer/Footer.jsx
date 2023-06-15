25af75f38e74154c695e60fb7199d347c440a5a0
aabbcc <iyranly@163.com>
2023-06-15 13:18:32 +0800

    [Feture]Add feedback link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,8 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 import { useTranslation } from 'react-i18next';
+import { currentLanguage } from '../../utils/i18n';
+import community_link from '../../assets/JsonData/hyper_link.json'
 
 
 const Footer = (props) => {
@@ -8,13 +10,13 @@ const Footer = (props) => {
     const [t] = useTranslation();
 
     const footer_res_config = [
+        /*{
+            display_name: "footer.feedback",
+            link: "https://talk.nervos.org/t/dis-ckbfans-ckbdapps-com-ckbfans-community-grant-proposal-ckbdapps-com/7004/29"
+        },*/
         {
-            display_name:"footer.feedback",
-            link:"https://talk.nervos.org/t/dis-ckbfans-ckbdapps-com-ckbfans-community-grant-proposal-ckbdapps-com/7004/29"
-        },
-        {
-            display_name:"footer.followus",
-            link:"https://twitter.com/@ckbmeta"
+            display_name: "footer.followus",
+            link: "https://twitter.com/@ckbmeta"
         }
     ]
 
@@ -23,21 +25,24 @@ const Footer = (props) => {
     }, []);
 
     return (<div className='flex flex-col place-content-center gap-4'>
-                <span className='border-[0.1px] seperator'></span>
-                <div className=' text-sm px-3 flex items-center justify-center'>
-                    {
-                        footer_res_config.map((item, index) => {
-                            return <a key={index} className=' text-sm px-3 py-5' href={item.link} rel="noopener noreferrer" target="_blank">
-                            {t(item.display_name)}
-                            </a>
-                        })
-                    }
-                </div>
-                
-                <div className=' text-sm px-3 py-5 flex items-center justify-center'>
-                © 2023 ckbdapps.com All rights reserved
-                </div>
-            </div>
+        <span className='border-[0.1px] seperator'></span>
+        <div className=' text-sm px-3 flex items-center justify-center'> 
+            <a className=' text-sm px-3 py-5' href={community_link.community[currentLanguage()].feedback.link} rel="noopener noreferrer" target="_blank">
+                {community_link.community[currentLanguage()].feedback.name}
+            </a>
+            {
+                footer_res_config.map((item, index) => {
+                    return <a key={index} className=' text-sm px-3 py-5' href={item.link} rel="noopener noreferrer" target="_blank">
+                        {t(item.display_name)}
+                    </a>
+                })
+            }
+        </div>
+
+        <div className=' text-sm px-3 py-5 flex items-center justify-center'>
+            © 2023 ckbdapps.com All rights reserved
+        </div>
+    </div>
     )
 }
 
